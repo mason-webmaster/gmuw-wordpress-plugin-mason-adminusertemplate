53176e23e95c9cb034e47c035bbe5dcc9d53059a
Jan Macario <jmacario@gmu.edu>
2025-03-05 17:58:06 -0500

    Modifies the user role that will automatically have their dashboard set up to match that of the template user from subscriber to the custom mason_user role.

--- php/users.php
+++ php/users.php
@@ -8,8 +8,8 @@
 add_action( 'user_register', 'gmuw_aut_new_user', 10, 1 );
 function gmuw_aut_new_user( $user_id ) {
 
-	//if user is subscriber
-	if ( in_array( 'subscriber', (array) get_userdata($user_id)->roles ) ) {
+	//if user is mason_user
+	if ( in_array( 'mason_user', (array) get_userdata($user_id)->roles ) ) {
 
 		//set up dashboard
 		gmuw_reset_dash($user_id);
